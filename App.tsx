50bb65ffebbdd2e0709976c931951eaad10a30c4
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2023-01-13 23:30:08 -0300

    chore: remove unnecessary code from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import React from 'react'
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-import { ThemeProvider } from "styled-components";
-import Login from "./src/modules/Login/screens/Login";
 import { HomeView } from "./src/modules/Home/screens/HomeView";
-import { light } from "./src/themes";
 
 export default function App() {
   return (
-    <ThemeProvider theme={light}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        {/* <Login /> */}
-        <HomeView />
-      </View>
-    </ThemeProvider>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <HomeView />
+    </View>
   );
 }
 
